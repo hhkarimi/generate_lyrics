5a43e25c1755d818790faa10ffc3839700b8bb26
hhkarimi <hussain.h.karimi@gmail.com>
2018-09-08 11:31:54 -0400

    Simplified header removal

--- notebooks/get_lyrics.py
+++ notebooks/get_lyrics.py
@@ -44,12 +44,31 @@ def get_artists_songs(genius_client, artists_names, verbose=False, overwrite=Fal
             genius_client.save_artists(artist_object, filename=filename, overwrite=overwrite)
 
 
+def remove_headers(artists_names):
+    for artist_name in artists_names:
+
+        # open file
+        filename = './data/lyrics_by_artist/{}.json'.format(artist_name.replace(' ',''))
+        with open(filename) as f:
+            data = json.load(f)
+
+        # remove headers
+        for i in range(0, len(data['artists'][0]['songs'])):
+            for header in ['Chorus', 'Refrain', 'Verse', 'Bridge', 'Break', 'Intro', 'Outro', '[', ']', ':',
+                           '(', ')', 'x2', 'x3', 'x4', 'x5']:
+                data['artists'][0]['songs'][i]['lyrics'] = data['artists'][0]['songs'][i]['lyrics'].replace(header, '')
+
+        # save file
+        with open(filename, 'w') as f:
+            json.dump(data, f)
+
+
 def main():
     logger = set_logger()
     args = parse_args()
     client = set_client(client_access_token)
     get_artists_songs(client, args.artists_names, verbose=args.verbose, overwrite=args.overwrite)
-
+    remove_headers(args.artists_names)
 
 if __name__ == '__main__':
     main()
